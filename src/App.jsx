f44d50d78241854d9e6d8a80203d628c989da85a
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2026-01-21 12:34:27 +0200

    js begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 const App = () => {
-  return (
-  <>
 
-Hello, World!!
-    </>
+    let a = "World's from Che"
+    let b = 2026
+    const isValid = false
+
+    if (isValid) {
+        a
+    } else {
+        b
+    }
+
+    return (
+      <>
+  <h1>
+
+
+Hello, {a} !!
+    </h1>
+      <p>
+          якийсь текст
+
+          { isValid ? a : b + 2}
+
+      </p>
+          </>
   )
 }
 
